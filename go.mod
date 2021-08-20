e55cac385bee6c259ef9ffbf6f926af9d263b540
Eugene Lepeico <eugenelepeico@gmail.com>
2021-08-20 08:28:51 +0300

    feat: add ctx as first param

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/lepeico/signal
 
 go 1.16
+
+require (
+	github.com/pkg/errors v0.9.1
+	golang.org/x/sys v0.0.0-20210819135213-f52c844e1c1c
+)
--- signal.go
+++ signal.go
@@ -1,20 +1,40 @@
 package signal
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"os/signal"
+
+	"github.com/pkg/errors"
 )
 
-func WaitForTermination(afterTermination ...func() error) error {
-	intChan := make(chan os.Signal, 1)
-	signal.Notify(intChan, _sigINT, _sigTERM)
-	<-intChan
+func WaitForTermination(ctx context.Context, afterTermination ...func() error) (err error) {
+	sigChan := make(chan os.Signal, 1)
+
+	signal.Notify(sigChan, _sigINT, _sigTERM)
+	defer signal.Stop(sigChan)
+
+	select {
+	case sig := <-sigChan:
+		err = Error{sig}
+	case <-ctx.Done():
+		err = ctx.Err()
+	}
+
+	if err != nil {
+		return errors.Wrap(err, "listening to closing chan")
+	}
 
 	for _, f := range afterTermination {
-		if err := f(); err != nil {
+		if err = f(); err != nil {
 			return err
 		}
 	}
 
 	return nil
 }
+
+type Error struct{ os.Signal }
+
+func (e Error) Error() string { return fmt.Sprintf("receiving signal %s", e.Signal) }
